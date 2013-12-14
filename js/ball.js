75d007396b3e950fd11d5dc526d2e3aec780ca47
jmsv23 <jmsv23@gmail.com>
2013-12-14 16:40:45 -0600

    ball bounce

--- js/ball.js
+++ js/ball.js
@@ -10,6 +10,16 @@ var Ball = pulse.Sprite.extend(	{
 		var newX = this.position.x + this.velocity.x * (enlapsedMS / 1000);
 		var newY = this.position.y + this.velocity.y * (enlapsedMS / 1000);
 
+		if(newX - (this.size.width / 2) <= 0) {
+			newX = this.size.width / 2;
+			this.velocity.x *= -1;
+		}
+
+		if(newY - (this.size.height / 2) <= 0) {
+			newY = this.size.height / 2;
+			this.velocity.y *= -1;
+		}
+
 		this.position.x = newX;
 		this.position.y = newY;
 
